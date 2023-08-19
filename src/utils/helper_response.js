3ee2e69073b3fd1345c401d8585985dcf8875093
Doni Putra <d.putra@global-coding.com>
2023-08-19 21:57:06 +0700

    feat : add helper response format

---
+++ src/utils/helper_response.js
@@ -0,0 +1,17 @@
+// This is global response pattern
+export function response(code, message, data) {
+    return {
+        code,
+        message,
+        data
+    }
+}
+
+export function responseWithoutData(code, message) {
+    return {
+        code,
+        message
+    }
+}
+
+
