8117e050b8a0e61a1bcce9c88615e56caa60525b
Doni Putra <d.putra@global-coding.com>
2024-10-13 00:51:35 +0700

    feat: add and update router admin related

--- src/routes/cms/admin.js
+++ src/routes/cms/admin.js
@@ -1,19 +1,23 @@
 import express from 'express';
 import * as controller from '../../controllers/admin.js';
-import { authenticateTokenAdmin } from '../../middlewares/auth.js';
+import { authenticateTokenAdmin } from '../../middlewares/appAuth.js';
+import { checkBody } from '../../middlewares/bodyChecker.js'
 
 const router = express.Router();
 
-router.post('/login', controller.login);
-router.post('/create-test', controller.createAdmin);
-router.post('/', authenticateTokenAdmin, controller.createAdmin);
-router.get('/', authenticateTokenAdmin, controller.getAdmins);
-router.get('/:adminId', authenticateTokenAdmin, controller.getAdmin);
-router.put('/:adminId', authenticateTokenAdmin, controller.updateAdmin);
-router.delete('/:adminId', authenticateTokenAdmin, controller.deleteAdmin);
-router.delete('/:adminId/permanent', authenticateTokenAdmin, controller.deleteAdminPermanent);
+router.get('/', authenticateTokenAdmin, controller.getAllAdmins);
+router.get('/:id', authenticateTokenAdmin, controller.getAdminById);
+router.post('/', authenticateTokenAdmin, checkBody, controller.createAdmin);
 
-router.post('/forgot-password', controller.forgotPassword);
-router.patch('/reset-password/:resetToken', controller.resetPassword);
+router.post('/login', checkBody, controller.login);
+router.post('/forgot-password', checkBody, controller.forgotPassword);
+router.patch('/reset-password/:resetPassToken', checkBody, controller.resetPassword);
+router.post('/verify-token-auth', checkBody, controller.verifyAdminTokenAuth);
+router.patch('/:id/update-password', authenticateTokenAdmin, checkBody, controller.updatePassword);
+
+router.post('/create-test', controller.createAdmin);
+router.put('/:id', authenticateTokenAdmin, controller.updateAdmin);
+router.delete('/:id', authenticateTokenAdmin, controller.deleteAdmin);
+router.delete('/:id/permanent', authenticateTokenAdmin, controller.deleteAdminPermanent);
 
 export default router;
