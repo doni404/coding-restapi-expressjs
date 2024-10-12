98b0740d341458c7292e3e8e78e7ea433c69d8a6
Doni Putra <d.putra@global-coding.com>
2024-10-13 00:47:01 +0700

    fix: change back from the star

--- src/routes/cms/customer_store.js
+++ src/routes/cms/customer_store.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import * as controller from '../../controllers/customer_store.js';
-import { authenticateTokenAdmin } from '../../middlewares/auth.js';
+import { authenticateTokenAdmin } from '../../middlewares/appAuth.js';
 
 const router = express.Router();
 
--- src/routes/cms/supplier_order.js
+++ src/routes/cms/supplier_order.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import * as controller from '../../controllers/supplier_order.js';
-import { authenticateTokenAdmin } from '../../middlewares/auth.js';
+import { authenticateTokenAdmin } from '../../middlewares/appAuth.js';
 
 const router = express.Router();
 
